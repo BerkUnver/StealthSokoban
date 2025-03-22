d3e53bd4d1277932014fe8c42980f9ab95d17f0e
Berk Unver <berkunver03@gmail.com>
2025-03-22 17:36:19 -0400

    Got exported blender meshes with vertex indices to show up in the game.

--- blender/my_mesh_exporter.py
+++ blender/my_mesh_exporter.py
@@ -2,30 +2,63 @@ import bpy
 import bpy_extras
 import struct
 
+
+class ToExport():
+    def __init__(self, object, mesh, mesh_to_world):
+        self.object = object
+        self.mesh = mesh
+        self.mesh_to_world = mesh_to_world
+
+
 def export_my_mesh(context, filepath):
     f = open(filepath, "wb")
     
+    to_export_array = []
+    
+    for object in context.scene.collection.all_objects:
+        if object.type == "MESH":
+            to_export_array.append(ToExport(object, object.to_mesh(), object.matrix_world))
+            # TODO: Filter out hidden meshes
+            
     vertex_count = 0
-    for mesh in bpy.data.meshes:
-        vertex_count += len(mesh.vertices)
-    s = struct.pack("<Q", vertex_count)
+    index_count = 0
+    
+    print(f"Mesh count: {len(to_export_array)}")
+    
+    for to_export in to_export_array:
+        vertex_count += len(to_export.mesh.loops)
+        index_count += len(to_export.mesh.loop_triangles)
+    index_count *= 3 # 3 verts per triangle
+    
+    print(f"Index count: {index_count}")
+    print(f"Vertex count: {vertex_count}")
+    
+    s = struct.pack("<II", index_count, vertex_count)
     f.write(s)
     
-    for mesh in bpy.data.meshes:
-        for vertex in mesh.vertices:
-            s = struct.pack("<fff", vertex.co.x, vertex.co.z, vertex.co.y)
+    for to_export in to_export_array:
+        for tri in to_export.mesh.loop_triangles:
+            s = struct.pack("<3I", *tri.loops)
             f.write(s)
-    
-    # Because python, it keeps inserting a newline when I close the file.
-    # I will just ignore it for now, I do not want to spend time to figure it out.
+        
+    for to_export in to_export_array:
+        for loop in to_export.mesh.loops:
+            vertex = to_export.mesh.vertices[loop.vertex_index]
+            position = to_export.mesh_to_world @ vertex.co
+            s = struct.pack("<3f", position.x, position.z, position.y)
+            f.write(s)
+
     f.close()
+    
+    for to_export in to_export_array:
+        to_export.object.to_mesh_clear()
 
     return {'FINISHED'}
 
 
 class MyMeshExporter(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
     bl_idname = "export.my_mesh_exporter"
-    bl_label = "Export Triangle Mesh"
+    bl_label = "Export My Mesh"
 
     filename_ext = ".my_mesh"
 
