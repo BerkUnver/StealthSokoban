38e11d8ada4ca6f1274813ffaa52d6c23734b715
Berk Unver <berkunver03@gmail.com>
2025-03-23 00:10:03 -0400

    Got a player mesh to import into the game. Worked on adding UVs to the mesh format.

--- blender/my_mesh_exporter.py
+++ blender/my_mesh_exporter.py
@@ -10,16 +10,16 @@ class ToExport():
         self.mesh_to_world = mesh_to_world
 
 
-def export_my_mesh(context, filepath):
-    f = open(filepath, "wb")
-    
+def export_my_mesh(context, operator):
     to_export_array = []
     
     for object in context.scene.collection.all_objects:
         if object.type == "MESH":
             to_export_array.append(ToExport(object, object.to_mesh(), object.matrix_world))
             # TODO: Filter out hidden meshes
-            
+    
+
+    data = bytearray()
     vertex_count = 0
     index_count = 0
     
@@ -34,26 +34,39 @@ def export_my_mesh(context, filepath):
     print(f"Vertex count: {vertex_count}")
     
     s = struct.pack("<II", index_count, vertex_count)
-    f.write(s)
+    data.extend(s)
     
     for to_export in to_export_array:
         for tri in to_export.mesh.loop_triangles:
             s = struct.pack("<3I", *tri.loops)
-            f.write(s)
+            data.extend(s)
         
     for to_export in to_export_array:
-        for loop in to_export.mesh.loops:
+        vertex_count = len(to_export.mesh.loops)
+        if not to_export.mesh.uv_layers:
+            operator.report({"ERROR"}, "There's a mesh without uv layers in this scene. We don't support that.")
+            return {"CANCELLED"}
+
+        if len(to_export.mesh.uv_layers) > 1:
+            operator.report({"ERROR"}, "There's a mesh with more than 1 uv layer in the scene. We don't support that.")
+            return {"CANCELLED"}
+
+        uvs = to_export.mesh.uv_layers[0].uv
+        for i in range(vertex_count):
+            loop = to_export.mesh.loops[i]
+            uv = uvs[i].vector
+            
             vertex = to_export.mesh.vertices[loop.vertex_index]
             position = to_export.mesh_to_world @ vertex.co
-            s = struct.pack("<3f", position.x, position.z, position.y)
-            f.write(s)
-
-    f.close()
+            
+            s = struct.pack("<5f", position.x, position.z, position.y, uv.x, uv.y)
+            data.extend(s)
     
-    for to_export in to_export_array:
-        to_export.object.to_mesh_clear()
+    f = open(operator.filepath, "wb")
+    f.write(data)
+    f.close()
 
-    return {'FINISHED'}
+    return {"FINISHED"}
 
 
 class MyMeshExporter(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
@@ -69,7 +82,7 @@ class MyMeshExporter(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
     )
 
     def execute(self, context):
-        return export_my_mesh(context, self.filepath)
+        return export_my_mesh(context, self)
 
 
 def menu_func_export(self, context):
